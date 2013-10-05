97ded205a3900bf2e3437613519741811cad6bde
Zhong Guan <guanzh.email@gmail.com>
2013-10-05 21:01:33 +0800

    update

---
+++ workspace/study/src/main/java/study/jaxrs/helloworld/HelloWorldImpl.java
@@ -0,0 +1,19 @@
+package study.jaxrs.helloworld;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@ApplicationScoped
+@Path("/helloworld")
+public class HelloWorldImpl {
+
+    @GET
+    @Produces("text/html")
+	public String getHtml() {
+		System.out.println(this);
+		return "<html lang=\"en\"><body><h1>Hello, World!!</h1></body></html>";
+	}
+
+}
