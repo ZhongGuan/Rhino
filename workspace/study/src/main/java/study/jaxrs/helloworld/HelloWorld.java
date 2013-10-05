74966713a0fb3693877cbab9bc6e6451faeff2d3
Zhong Guan <guanzh.email@gmail.com>
2013-10-05 22:00:26 +0800

    add xml,json using jaxb

--- workspace/study/src/main/java/study/jaxrs/helloworld/HelloWorld.java
+++ workspace/study/src/main/java/study/jaxrs/helloworld/HelloWorld.java
@@ -1,19 +1,62 @@
 package study.jaxrs.helloworld;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import study.jaxrs.helloworld.model.Customer;
+import study.jaxrs.helloworld.model.PhoneNumber;
 
 @ApplicationScoped
 @Path("/helloworld")
 public class HelloWorld {
 
     @GET
-    @Produces("text/html")
+    @Path("getHtml")
+    @Produces(MediaType.TEXT_HTML)
 	public String getHtml() {
 		System.out.println(this);
 		return "<html lang=\"en\"><body><h1>Hello, World!!</h1></body></html>";
 	}
-
+    
+    @GET
+    @Path("getCustomerXml")
+    @Produces(MediaType.APPLICATION_XML)
+	public Customer getCustomerXml() {
+    	Customer customer = new Customer();
+        customer.setId(1);
+        customer.setName("Zhong Guan");
+         
+        PhoneNumber pn = new PhoneNumber();
+        pn.setType("work");
+        pn.setValue("5551111");
+        List<PhoneNumber> phoneNumbers = new ArrayList<>();
+        phoneNumbers.add(pn);
+        customer.setPhoneNumbers(phoneNumbers);
+        
+        return customer;
+	}
+    
+    @GET
+    @Path("getCustomerJson")
+    @Produces(MediaType.APPLICATION_JSON)
+	public Customer getCustomerJson() {
+    	Customer customer = new Customer();
+        customer.setId(1);
+        customer.setName("Zhong Guan");
+         
+        PhoneNumber pn = new PhoneNumber();
+        pn.setType("work");
+        pn.setValue("5551111");
+        List<PhoneNumber> phoneNumbers = new ArrayList<>();
+        phoneNumbers.add(pn);
+        customer.setPhoneNumbers(phoneNumbers);
+        
+        return customer;
+	}
 }
---
+++ workspace/study/src/main/java/study/jaxrs/helloworld/model/Customer.java
@@ -0,0 +1,43 @@
+package study.jaxrs.helloworld.model;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Customer {
+	
+	private int id;
+	
+	private String name;
+	
+	private List<PhoneNumber> phoneNumbers;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@XmlElementWrapper
+	@XmlElement(name = "phoneNumber")
+	public List<PhoneNumber> getPhoneNumbers() {
+		return phoneNumbers;
+	}
+
+	public void setPhoneNumbers(List<PhoneNumber> phoneNumbers) {
+		this.phoneNumbers = phoneNumbers;
+	}
+}
